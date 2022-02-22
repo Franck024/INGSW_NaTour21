e9b249a88115741f02055c4f50148b7b50815cb1
Franck024 <fcozzolino24@gmail.com>
2022-02-22 19:18:55 +0100

    Creazione frame Registrazione
    Creato il frame di registrazione "frame_registrazione"

--- 3_CodiceSorgente/IngSW_NaTour21/app/src/main/java/com/example/natour21/MainActivity.java
+++ 3_CodiceSorgente/IngSW_NaTour21/app/src/main/java/com/example/natour21/MainActivity.java
@@ -10,5 +10,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.frame_login);
+
+        /* TEST */
+     //   setContentView(R.layout.frame_registrazione);
+        /* ------  */
     }
 }

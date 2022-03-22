90117e7ca0b6b7759ed9dd8784910f7f2fffa1a5
UNILucaM <lu.malangone@studenti.unina.it>
2022-03-22 19:10:08 +0100

    Aggiunte funzionalità relative alle mappe.

---
+++ 3_CodiceSorgente/IngSW_NaTour21/app/src/main/java/com/example/natour21/exceptions/ProviderDisabledException.java
@@ -0,0 +1,7 @@
+package com.example.natour21.exceptions;
+
+public class ProviderDisabledException extends Exception{
+    public ProviderDisabledException(String message){
+        super(message);
+    }
+}

9afae7e9cb9f453ef1ffcc8745a794bdcce045f9
Franck024 <fcozzolino24@gmail.com>
2022-03-23 21:14:31 +0100

    Creato accesso admin
    - creata voce nel login per effettuare accesso admin
    - creato frame accesso admin
    - prototipo statistiche
    - Controller accesso admin
    - prototipo controller statistiche

---
+++ 3_CodiceSorgente/IngSW_NaTour21/app/src/main/java/com/example/natour21/Controller_Statistiche.java
@@ -0,0 +1,14 @@
+package com.example.natour21;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Controller_Statistiche extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.frame_statistiche);
+    }
+}

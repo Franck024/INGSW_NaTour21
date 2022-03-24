f28e0490472dc41fffcf281b9bd85e86e033559d
Franck024 <fcozzolino24@gmail.com>
2022-03-24 12:59:57 +0100

    Creazione statistiche
    implementato grafico per statistiche
    implementato il controller statistiche

--- 3_CodiceSorgente/IngSW_NaTour21/app/src/main/java/com/example/natour21/Controller_Statistiche.java
+++ 3_CodiceSorgente/IngSW_NaTour21/app/src/main/java/com/example/natour21/Controller_Statistiche.java
@@ -1,8 +1,17 @@
 package com.example.natour21;
 
+import android.graphics.Color;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.utils.ColorTemplate;
+
+import java.util.ArrayList;
 
 public class Controller_Statistiche extends AppCompatActivity {
 
@@ -10,5 +19,30 @@ public class Controller_Statistiche extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.frame_statistiche);
+
+        BarChart barchart = findViewById(R.id.barChart);
+
+        ArrayList<BarEntry> visitors = new ArrayList<>();
+        /// estrarre valori dal db e inserirli sull'asse y
+        visitors.add(new BarEntry(1, 10));
+        visitors.add(new BarEntry(2, 15));
+        visitors.add(new BarEntry(3, 27));
+        visitors.add(new BarEntry(4, 9));
+        visitors.add(new BarEntry(5, 0));
+        visitors.add(new BarEntry(6, 0));
+
+        BarDataSet barDataSet = new BarDataSet(visitors, "Conteggio");
+        barDataSet.setColors(ColorTemplate.MATERIAL_COLORS);
+        barDataSet.setValueTextColor(Color.BLACK);
+        barDataSet.setValueTextSize(16f);
+
+        BarData barData = new BarData(barDataSet);
+
+        barchart.setFitBars(true);
+        barchart.setData(barData);
+        barchart.getDescription().setText("Statistiche");
+        barchart.animateY(2000);
+
+
     }
 }

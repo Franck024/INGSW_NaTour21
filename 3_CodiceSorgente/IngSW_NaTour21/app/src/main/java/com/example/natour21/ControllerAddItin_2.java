07fd632ab1306adb085b459d575b29737beed0fc
Franck024 <fcozzolino24@gmail.com>
2022-03-24 14:05:22 +0100

    Frame Map

--- 3_CodiceSorgente/IngSW_NaTour21/app/src/main/java/com/example/natour21/ControllerAddItin_2.java
+++ 3_CodiceSorgente/IngSW_NaTour21/app/src/main/java/com/example/natour21/ControllerAddItin_2.java
@@ -2,20 +2,33 @@ package com.example.natour21;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.natour21.map.MapActivity;
+
 public class ControllerAddItin_2 extends AppCompatActivity {
     Button indietro;
+    TextView apriMappa;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.frame_aggiungi_itinerario2);
 
         indietro = findViewById(R.id.btnIndietro2);
+        apriMappa = findViewById(R.id.openMap);
 
         indietro.setOnClickListener(v -> startActivity(new Intent(ControllerAddItin_2.this, ControllerAddItin.class)));
+        apriMappa.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(ControllerAddItin_2.this, MapActivity.class));
+            }
+        });
 
     }
 }
